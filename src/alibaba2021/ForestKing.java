7ef18920521563a9fb3720751015eee6718f59d2
sswen <799438891@qq.com>
2022-03-10 20:09:30 +0800

    forest king

--- src/alibaba2021/ForestKing.java
+++ src/alibaba2021/ForestKing.java
@@ -1,25 +1,23 @@
 package alibaba2021;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class ForestKing {
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
-        int[] idol = new int[n+1];
-        int[] res = new int[n+1];
-        for(int i = 1; i < idol.length; i++){
-            idol[i] = sc.nextInt();
-        }
-        for(int i = 1; i < idol.length; i++){
-            int index = i;
-            do{
-                res[index]++;
-                index = idol[index];
-            }while (index != 0);
-        }
-        for(int i = 1; i < res.length; i++){
-            System.out.println(res[i]);
-        }
+        int[] idol = new int[n + 1];
+        int[] tickets = new int[n + 1];
+
+        Arrays.fill(tickets, 1);
+        for(int i = 1; i < idol.length; i++) idol[i] = sc.nextInt();
+
+        for(int i = tickets.length-1; i >= 1; i--)
+            tickets[idol[i]] += tickets[i];   // 有崇拜对象，假设把自己身上所有的票都投给崇拜对象
+        for(int i = 1; i <= n; i++)
+            System.out.println(tickets[i]);
     }
 }
