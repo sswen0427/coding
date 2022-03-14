f8d786af67e1bdbc89683c7634b044bf131b5274
wss <799438891@qq,com>
2022-03-14 23:42:44 +0800

    Insert, delete, and random access containers with O(1) time complexity

---
+++ src/swordpointtooffer/O1Containter.java
@@ -0,0 +1,49 @@
+package swordpointtooffer;
+
+import java.util.*;
+
+public class O1Containter {
+    private Map<Integer, Integer> num_idx = new HashMap<>();
+    private List<Integer> nums = new ArrayList<>();
+    Random random = new Random();
+
+    /** Initialize your data structure here. */
+//        public RandomizedSet() {
+//
+//        }
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if(num_idx.containsKey(val))  return false;
+        int idx = nums.size();
+        num_idx.put(val, idx);
+        nums.add(val);
+        return true;
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if(!num_idx.containsKey(val))  return false;
+        int idx = num_idx.get(val);
+        int lastNum = nums.get(nums.size() -1);
+        nums.set(idx, lastNum);
+        nums.remove(nums.size()-1);
+        num_idx.put(lastNum, idx);
+        num_idx.remove(val);
+        return true;
+    }
+
+    /** Get a random element from the set. */
+    public int getRandom() {
+        int idx = random.nextInt(nums.size());
+        return nums.get(idx);
+    }
+
+    /**
+     * Your RandomizedSet object will be instantiated and called as such:
+     * RandomizedSet obj = new RandomizedSet();
+     * boolean param_1 = obj.insert(val);
+     * boolean param_2 = obj.remove(val);
+     * int param_3 = obj.getRandom();
+     */
+}
--- src/swordpointtooffer/README.md
+++ src/swordpointtooffer/README.md
@@ -37,3 +37,6 @@ cnt 的元素值之和为 0。由于 cnt 的值不为正，元素值之和为 0 
 &nbsp;&nbsp;&nbsp;&nbsp;s[0] == '0': zero[0] = 0, one[0] = 1  
 &nbsp;&nbsp;&nbsp;&nbsp;s[0] == '1': zero[0] = 1, one[0] = 0  
 &nbsp;&nbsp;&nbsp;&nbsp;由于当前的最小值只和前一位的最小值有关，所以可以优化空间复杂度为O(1)，只使用两个变量zero和one来表示前面的最小值
+
+剑指 Offer II 030. [插入、删除和随机访问都是 O(1) 的容器](https://leetcode-cn.com/problems/FortPu/)  
+&nbsp;&nbsp;&nbsp;&nbsp;无序字典+动态数组.思想类似于对堆进行操作（1）插入一个数时，先放到最后（2）删除一个数时，让最后一个数字去替换
