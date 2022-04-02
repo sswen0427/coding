f4f730388850ec16201d218bbe56fb1096473e64
wss <799438891@qq,com>
2022-04-02 23:55:01 +0800

    anagram

---
+++ src/leetcode/seq101_110/LevelOrder.java
@@ -0,0 +1,56 @@
+package leetcode.seq101_110;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class LevelOrder {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null){
+            return res;
+        }
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.addLast(root);
+        while (queue.size() > 0){
+            int length = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0; i < length; i++){
+                TreeNode node = queue.pollFirst();
+                list.add(node.val);
+                if(node.left != null) queue.addLast(node.left);
+                if(node.right != null) queue.addLast(node.right);
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
