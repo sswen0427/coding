9b3bdc6842c9b1fb35b933619e3bffde91014a1c
wss <799438891@qq,com>
2022-05-31 01:10:33 +0800

    top k frequent

---
+++ src/leetcode/seq341_350/TopKFrequent.java
@@ -0,0 +1,33 @@
+package leetcode.seq341_350;
+
+import java.util.*;
+
+public class TopKFrequent {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
+            if(map.get(o1) > map.get(o2)){
+                return 1;
+            }else if(map.get(o1) < map.get(o2)){
+                return -1;
+            }else{
+                return 0;
+            }
+        });
+        for(Integer key : map.keySet()){
+            queue.add(key);
+            if(queue.size() > k){
+                queue.poll();
+            }
+        }
+
+        int[] res = new int[k];
+        for(int i = 0; i < k; i++){
+            res[i] = queue.poll();
+        }
+        return res;
+    }
+}
