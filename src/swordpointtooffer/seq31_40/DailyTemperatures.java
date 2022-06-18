e92df742debd5916c42c476d708a32bfbbbbf033
wss <799438891@qq,com>
2022-06-18 12:21:17 +0800

    DailyTemperatures

---
+++ src/swordpointtooffer/seq31_40/DailyTemperatures.java
@@ -0,0 +1,21 @@
+package swordpointtooffer.seq31_40;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class DailyTemperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> stack = new LinkedList<>();
+        int length = temperatures.length;
+        int[] res = new int[length];
+
+        for(int i = 0; i < length; i++){
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
+                int index = stack.pop();
+                res[index] = i - index;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
--- src/swordpointtooffer/seq31_40/README.md
+++ src/swordpointtooffer/seq31_40/README.md
@@ -1,2 +1,5 @@
 剑指 Offer II 031. [最近最少使用缓存](https://leetcode.cn/problems/OrIXps/)  
 &nbsp;&nbsp;&nbsp;&nbsp;哈希表 + 双向链表， 照着抄的
+
+剑指 Offer II 038. [每日温度](https://leetcode.cn/problems/iIQa4I/)  
+&nbsp;&nbsp;&nbsp;&nbsp;单调栈, 发现自己知道用什么方法却不会写代码
