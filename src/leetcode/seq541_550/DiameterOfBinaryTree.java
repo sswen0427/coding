3a2950432461eb25f166cfe1c0516ef475132a0f
wss <799438891@qq,com>
2022-06-21 17:43:23 +0800

    DiameterOfBinaryTree

---
+++ src/leetcode/seq541_550/DiameterOfBinaryTree.java
@@ -0,0 +1,47 @@
+package leetcode.seq541_550;
+
+public class DiameterOfBinaryTree {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    private int ans = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    private int dfs(TreeNode node){
+        if(node == null){
+            return 0;
+        }
+        int L = dfs(node.left);
+        int R = dfs(node.right);
+        ans = Math.max(ans, L+R);
+        return Math.max(L, R) + 1;
+    }
+}
---
+++ src/leetcode/seq541_550/README.md
@@ -0,0 +1,3 @@
+543 [二叉树的直径](https://leetcode.cn/problems/diameter-of-binary-tree/)  
+&nbsp;&nbsp;&nbsp;&nbsp;照着抄了一下
+
