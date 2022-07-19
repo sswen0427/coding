fc7590b6bf9c67f0528547da2207685ae198bd90
wss <799438891@qq,com>
2022-07-19 19:38:45 +0800

    MergeKLists

---
+++ src/leetcode/seq0021_0030/MergeKLists.java
@@ -0,0 +1,45 @@
+package leetcode.seq0021_0030;
+import java.util.PriorityQueue;
+
+public class MergeKLists {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode head = new ListNode();
+        ListNode work = head;
+        PriorityQueue<ListNode> queue = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.val, o2.val));
+
+        for(ListNode node : lists){
+            if(node != null){
+                queue.add(node);
+            }
+        }
+        while (!queue.isEmpty()){
+            ListNode node = queue.poll();
+            ListNode next = node.next;
+            if(next != null){
+                queue.add(next);
+            }
+            work.next = node;
+            work = work.next;
+        }
+        work.next = null;
+        return head.next;
+    }
+}
