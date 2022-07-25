c27c398e879c955d4ffa437573862518ac93e3f3
wss <799438891@qq,com>
2022-07-25 20:10:12 +0800

    max path sum

---
+++ src/swordpointtooffer2/seq051_060/MaxPathSum.java
@@ -0,0 +1,51 @@
+package swordpointtooffer2.seq051_060;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class MaxPathSum {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    int maxSum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        maxGain(root);
+        return maxSum;
+    }
+
+    private int maxGain(TreeNode node){
+        if(node == null) return 0;
+
+        int leftGain = Math.max(maxGain(node.left), 0);
+        int rightGain = Math.max(maxGain(node.right), 0);
+
+        int priceNewPath = node.val + leftGain + rightGain;
+        maxSum = Math.max(maxSum, priceNewPath);
+
+        return node.val + Math.max(leftGain, rightGain);
+    }
+}
---
+++ src/swordpointtooffer2/seq051_060/README.md
@@ -0,0 +1,5 @@
+
+剑指 Offer II 051. [节点之和最大的路径.](https://leetcode.cn/problems/jC7MId/)  
+&nbsp;&nbsp;&nbsp;&nbsp; 比较难
+
+
