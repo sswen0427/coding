c3c781559bb3b066c11b2498c5c2b07d0937ee63
wss <799438891@qq,com>
2022-07-28 22:01:30 +0800

    max profit

---
+++ src/leetcode/seq0121_0130/MaxProfit.java
@@ -0,0 +1,16 @@
+package leetcode.seq0121_0130;
+
+public class MaxProfit {
+    public int maxProfit(int[] prices) {
+        int minprice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < minprice){
+                minprice = prices[i];
+            }else if(prices[i] - minprice > maxProfit){
+                maxProfit = prices[i] - minprice;
+            }
+        }
+        return maxProfit;
+    }
+}
---
+++ src/leetcode/seq0121_0130/README.md
@@ -0,0 +1,2 @@
+121 [买卖股票的最佳时机](https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/)  
+&nbsp;&nbsp;&nbsp;&nbsp;第一次字节面试没做出来，后来又没做出来，恶心人
