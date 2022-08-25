47091db8f2426d0f9ff08548128c1adf45296880
sswen <799438891@qq.com>
2022-08-25 11:30:14 +0800

    has cycle

---
+++ src/leetcode/seq0141_0150/HasCycle.java
@@ -0,0 +1,33 @@
+package leetcode.seq0141_0150;
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class HasCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) return true;
+        }
+        return false;
+    }
+}
--- src/leetcode/seq0141_0150/README.md
+++ src/leetcode/seq0141_0150/README.md
@@ -1,2 +1,5 @@
+141 [环形链表](https://leetcode.cn/problems/linked-list-cycle/)  
+&nbsp;&nbsp;&nbsp;&nbsp;简单
+
 144 [二叉树前序遍历](https://leetcode.cn/problems/binary-tree-preorder-traversal/)  
 &nbsp;&nbsp;&nbsp;&nbsp;照着抄了一遍，感觉应该多复习
