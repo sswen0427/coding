e3577677bfa3490d40757dff5cd80d652d92329e
wss <799438891@qq,com>
2022-09-15 12:19:04 +0800

    refactor

---
+++ leetcode/src/main/java/leetcode/_0701_0800/seq0071_0080/Combine.java
@@ -0,0 +1,32 @@
+package leetcode._0701_0800.seq0071_0080;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class Combine {
+//    class Solution1{
+//
+//        private List<List<Integer>> lists = new ArrayList<>();
+//
+//        private List<Integer> list = new ArrayList<>();
+//
+//        private void dfs(int n, int k, int begin){
+//            if(list.size() == k){
+//                lists.add(new ArrayList<>(list));
+//                return;
+//            }
+//            for(int i = begin; i <= n; i++){
+//                list.add(i);
+//                dfs(n, k, i+1);
+//                list.remove(list.size()-1);
+//            }
+//        }
+//
+//
+//
+//    }
+//    public List<List<Integer>> combine(int n, int k) {
+//
+//    }
+}
